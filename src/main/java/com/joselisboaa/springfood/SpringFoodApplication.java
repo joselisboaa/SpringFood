d98b659d4bd1866d676e86236d3211281277e611
joselisboaa <joselisboaa05@gmail.com>
2023-12-25 19:08:20 -0300

    int: project first commit

---
+++ src/main/java/com/joselisboaa/springfood/SpringFoodApplication.java
@@ -0,0 +1,13 @@
+package com.joselisboaa.springfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joselisboaa/springfood/SpringFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joselisboaa.springfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
